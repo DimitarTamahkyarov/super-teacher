c5d763bccd59599d0349c8f93b0f74f7ef1dfc77
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2024-10-30 22:09:40 +0200

    First migration

--- SuperTeacher/SuperTeacher/settings.py
+++ SuperTeacher/SuperTeacher/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'SuperTeacher.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "super-teacher",
+        "USER": "postgres",
+        "PASSWORD": "112344567",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
